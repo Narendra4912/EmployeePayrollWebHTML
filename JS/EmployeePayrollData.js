36c384e508a70a2b968919cf9b307c9cb7692b5e
narendra4912 <narendra4912@gmail.com>
2022-01-25 10:49:13 +0530

    Day 44 UC-1 Added: Employee getters and setters added

---
+++ JS/EmployeePayrollData.js
@@ -0,0 +1,71 @@
+class EmployeePayrollData {
+  set id(id) {
+    this._id = id;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set profilePic(profilePic) {
+    this._profilePic = profilePic;
+  }
+
+  get profilePic() {
+    return this._profilePic;
+  }
+
+  set name(name) {
+    let nameRegex=RegExp('^[A-Z]{1}[A-Za-z0-9]{2}$');
+    if(nameRegex.test(name))
+        this._name = name;
+    throw "Name is incorrect !!!";
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set gender(gender) {
+    this._gender = gender;
+  }
+
+  get gender() {
+    return this._gender;
+  }
+
+  set department(department) {
+    this._department = department;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  set startDate(startDate) {
+      
+    this._startDate = startDate;
+  }
+
+  get startDate() {
+    return this._startDate;
+  }
+
+  set salary(salary) {
+    this._salary = salary;
+  }
+
+  get salary() {
+    return this._salary;
+  }
+
+  set notes(notes) {
+    this._notes = notes;
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+
+}
